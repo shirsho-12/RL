e9925a7f7055046a8cbfe4a3ecb4704cf80df652
Shirshajit Sen Gupta <shirshajit@gmail.com>
2021-07-05 13:52:53 +0600

    shirsho-12.github.io post edits

--- TD/taxi_service.py
+++ TD/taxi_service.py
@@ -3,6 +3,14 @@ from td_algos import q_learning, sarsa, double_q_learning
 
 import gym
 
+import sys
+import os
+print(os.getcwd())
+sys.path.append(os.getcwd())
+# print(sys.path)
+from text_recorder import record
+
+
 env = gym.make("Taxi-v3")
 num_actions, num_states = env.action_space.n, env.observation_space.n
 print(num_actions, num_states)
@@ -14,24 +22,26 @@ epsilon = 0.1
 behaviour_policy = gen_eps_greedy_policy(env.action_space.n, epsilon)
 
 optimal_Q, optimal_policy, info = q_learning(env, behaviour_policy, gamma, num_episodes, alpha)
-# print("\nQ-Learning Optimal policy: \n", optimal_policy)
-plot_rate(info["length"], info["rewards"], "Taxi Service: Q-Learning")
 
-num_episodes = 1000 
-gamma = 1
-alpha = 0.6
-epsilon = 0.01        # Grid Search parameters
-eps_policy = gen_eps_greedy_policy(env.action_space.n, epsilon)
-optimal_Q, optimal_policy, info = sarsa(env, gamma, num_episodes, alpha, eps_policy)
-# print("\nSARSA Optimal policy: \n", optimal_policy)
-plot_rate(info["length"], info["rewards"], "Taxi Service: SARSA")
+record(env, optimal_policy, './video/taxi.json')
+# # print("\nQ-Learning Optimal policy: \n", optimal_policy)
+# plot_rate(info["length"], info["rewards"], "Taxi Service: Q-Learning")
 
-num_episodes = 3000
-gamma = 1
-alpha = 0.4
-epsilon = 0.1       
-eps_policy = gen_eps_greedy_policy(env.action_space.n, epsilon)
+# num_episodes = 1000 
+# gamma = 1
+# alpha = 0.6
+# epsilon = 0.01        # Grid Search parameters
+# eps_policy = gen_eps_greedy_policy(env.action_space.n, epsilon)
+# optimal_Q, optimal_policy, info = sarsa(env, gamma, num_episodes, alpha, eps_policy)
+# # print("\nSARSA Optimal policy: \n", optimal_policy)
+# plot_rate(info["length"], info["rewards"], "Taxi Service: SARSA")
 
-optimal_Q, optimal_policy, info = double_q_learning(env, gamma, num_episodes, alpha, eps_policy)
-# print("\nDouble Q-Learning Optimal policy: \n", optimal_policy)
-plot_rate(info["length"], info["rewards"], "Taxi Service: Double Q-Learning")
+# num_episodes = 3000
+# gamma = 1
+# alpha = 0.4
+# epsilon = 0.1       
+# eps_policy = gen_eps_greedy_policy(env.action_space.n, epsilon)
+
+# optimal_Q, optimal_policy, info = double_q_learning(env, gamma, num_episodes, alpha, eps_policy)
+# # print("\nDouble Q-Learning Optimal policy: \n", optimal_policy)
+# plot_rate(info["length"], info["rewards"], "Taxi Service: Double Q-Learning")
--- text_recorder.py
+++ text_recorder.py
@@ -1,36 +1,43 @@
+# Using string_recorder https://github.com/kiyukuta/string_recorder.git for text-based environments
+import subprocess
 import gym
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
+import string_recorder
+
 def record(env, policy, video_dir):
+    rec = string_recorder.StringRecorder()  
+    timestep_limit = 30
+
     video_recorder = VideoRecorder(env, video_dir, enabled=True)
     is_done = False
     state = env.reset()
     while not is_done:        # Loop until game ends
         env.render()
+        # subprocess.call('clear', shell=False)
         video_recorder.capture_frame()                                  
         action = policy[state]
         state, reward, is_done, info = env.step(action)
     video_recorder.close()
     video_recorder.enabled = False
     env.close()
-
-
+    rec.make_gif_from_gym_record(video_dir)
 
 if __name__ == "__main__":
-    video_dir = './video/pacman.mp4'
-
-    env = gym.make("MsPacman-v0")
-    # env = gym.wrappers.Monitor(env, video_dir)
+    env = gym.make("Taxi-v3")
+    rec = string_recorder.StringRecorder()  
+    timestep_limit = 30
+    video_dir = "./video/taxi2.json"
     video_recorder = VideoRecorder(env, video_dir, enabled=True)
-
     is_done = False
-    env.reset()
+    state = env.reset()
     while not is_done:        # Loop until game ends
         env.render()
+        # subprocess.call('clear', shell=False)
         video_recorder.capture_frame()                                  
         action = env.action_space.sample()
-        new_space, reward, is_done, info = env.step(action)
-
+        state, reward, is_done, info = env.step(action)
     video_recorder.close()
     video_recorder.enabled = False
     env.close()
+    rec.make_gif_from_gym_record(video_dir)
